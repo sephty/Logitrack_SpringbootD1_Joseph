16d1928823f2869815627bd2927791912a8d875c
sephty <146395395+sephty@users.noreply.github.com>
2026-08-31 23:59:11 -0500

    Fix: Added proper Swagger version. currently checking for a 500 error within producto creation

--- inventory-service/src/main/java/com/Springboot_project/inventory_service/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/Springboot_project/inventory_service/InventoryServiceApplication.java
@@ -2,12 +2,13 @@ package com.Springboot_project.inventory_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InventoryServiceApplication.class, args);
+		SpringApplication.run(InventoryServiceApplication.class, args); System.out.println("HOLA MUNDO");
 	}
 
 }
